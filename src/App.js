27a8cfe6ed5471f973876e8e5a161c7383f1ff68
Eitan Pushett <eitan.pushett@cnvrg.io>
2022-12-14 07:37:33 +0200

    first

--- src/App.js
+++ src/App.js
@@ -4,16 +4,35 @@ import { useState } from 'react';
 
 function App() {
   const [toDo, setToDo] = useState('')
+  const [toDoList, setToDoList] = useState(['Buy Milk', 'Go to a movie', 'Pick up Anaelle', 'Sleep','Eat'])
+
+
 
   function handleToDoChange(e){
     setToDo(e.target.value)
   }
 
+    function AddItem(List){
+      setToDoList(toDoList.push(toDo))
+    }
+
+
+
 
   return (
     <div className="App">
       <input value={toDo} onChange = {handleToDoChange}/>
-      <button>Add to list</button>
+      <button onClick={AddItem}>Add to list</button>
+      <h3>To Do List:</h3>
+      <ul>
+        {toDoList.map(
+          toDo => (
+            <li>{toDo}</li>
+          )
+        )
+          
+        }
+      </ul>
     </div>
   );
 }
